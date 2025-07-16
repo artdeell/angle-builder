71227704ace0a42edabae942beee3cf5ebbda312
Maksim Belov <45949002+artdeell@users.noreply.github.com>
2025-07-16 23:00:19 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 git clone --depth 1 https://chromium.googlesource.com/chromium/tools/depot_tools.git
 export PATH=$PWD/depot_tools/:$PATH
+mkdir angle
+cd angle
 fetch angle
 echo "target_os = ['android']" >> .gclient
 gclient sync
 mkdir out
-mkdir build_out
+mkdir ../build_out
